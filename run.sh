98557bf58292f9fce5d4227ca3d23cf1f8fcc862
dank3k <116195702+dank3k@users.noreply.github.com>
2025-08-04 13:51:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 # Jalankan Nginx di background
+# `daemon off;` mencegah Nginx keluar setelah startup
+# dan `&` mengirimnya ke background
 nginx -g 'daemon off;' &
 
 # Jalankan Nginx Prometheus Exporter di background
-# Exporter akan scrape dari localhost pada endpoint /stub_status
+# Exporter mengambil metrik dari endpoint status Nginx
 /usr/local/bin/nginx-prometheus-exporter -nginx.scrape-uri=http://127.0.0.1/stub_status &
 
-# Tunggu hingga salah satu proses di atas selesai (misalnya, jika salah satu crash)
+# Perintah `wait -n` akan menunggu salah satu proses di atas selesai.
+# Ini mencegah container keluar jika salah satu layanan crash.
 wait -n
 
-# Keluar dengan kode status dari proses yang selesai
+# Keluar dengan kode status dari proses yang selesai pertama kali
 exit $?
